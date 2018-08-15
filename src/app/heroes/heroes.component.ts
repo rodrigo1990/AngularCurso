b2edc066c0d1c2109ed852ce5cc1a9ba18d77aa8
rodrigo1990 <mcd77.1990@gmail.com>
2018-08-15 17:06:30 -0300

    primera parte de servicios
    primera parte de servicios

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {Hero} from '../Hero';
+import { HEROES } from '../mock-heroes';
+import {HeroService} from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
@@ -6,12 +9,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-	hero;
-  constructor() { 
-  	this.hero="windstorm";
-  }
+	selectedHero: Hero;
+  heroes: Hero[];
+
+  constructor(private heroService: HeroService) {}
 
   ngOnInit() {
+    this.getHeroes();
+  }
+
+
+  onSelect(hero:Hero):void{
+   this.selectedHero = hero;
+  }
+
+  getHeroes():void{
+  this.heroes = this.heroService.getHeroes();
   }
 
 }
